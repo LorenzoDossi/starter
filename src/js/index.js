cf516f1ea4351461d91efd4200d7a18ddcc3da8d
Lorenzo Dossi <lorenzodss@gmail.com>
2021-07-18 19:00:36 +0200

    added locomotive barba and gsap

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,35 @@
 import DevUtils from './demo.js'
+import barba from '@barba/core'
+import gsap from 'gsap'
+import LocomotiveScroll from 'locomotive-scroll'
 
-const devUtils = new DevUtils();
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
 
-function alberto () {
-  console.log('ciao mi chiamo alberto');
-  let marco = 0
-  marco = 2
-}
+barba.hooks.before(() => {
+  scroll.stop()
+})
 
-alberto()
+barba.hooks.after((data) => {
+  scroll.update()
+  scroll.start()
+})
+
+barba.init({
+  preventRunning: true,
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    after(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
+});
